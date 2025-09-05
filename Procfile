b842c3d83cca86199edcf5d753575fa356cde3df
BrootBin <113306621+BrootBin@users.noreply.github.com>
2025-09-05 20:30:46 +0200

    переналаштування

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 web: cd sudoku && gunicorn sudoku_game.wsgi
-web: gunicorn sudoku_game.wsgi
+web: gunicorn sudoku_game.sudoku_game.wsgi
+
--- sudoku_game/sudoku_game/wsgi.py
+++ sudoku_game/sudoku_game/wsgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sudoku_game.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sudoku_game.sudoku_game.settings')
 
 application = get_wsgi_application()
+
 
 
